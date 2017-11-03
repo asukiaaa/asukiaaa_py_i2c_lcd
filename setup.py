e30d7deb8e72c589438c9cf48cd3f4ea695d34f3
asuki <asukiaaa@gmail.com>
2017-11-03 18:14:21 +0900

    Set readme as long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='asukiaaa_py_i2c_lcd',
     version='0.1.2',
     description='An i2c library to control AQM1602',
+    long_description=long_description,
     url='https://github.com/asukiaaa/asukiaaa_py_i2c_lcd',
     author='Asuki Kono',
     author_email='asukiaaa@gmail.com',
